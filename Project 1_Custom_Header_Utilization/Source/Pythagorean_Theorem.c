3b37a253644c13a532d47fad1d00ffba6c5cde59
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-09-21 09:11:09 -0400

    Update Pythagorean_Theorem.c

--- Project 1_Custom_Header_Utilization/Source/Pythagorean_Theorem.c
+++ Project 1_Custom_Header_Utilization/Source/Pythagorean_Theorem.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Custom function that has two integer parameters (a,b)
+// and returns an integer named c_squared
 int Pythagorean_Theorem (int a, int b)
 {
     int c_squared = (a*a) + (b*b);
