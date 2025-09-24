07bbd0dddadcbf85f9ee18c0783bf24740cb2c8b
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-09-24 19:30:38 -0400

    Delete Project 1_Custom_Header_Utilization directory

--- Project 1_Custom_Header_Utilization/Source/Pythagorean_Theorem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// Custom function that has two integer parameters (a,b)
-// and returns an integer named c_squared
-int Pythagorean_Theorem (int a, int b)
-{
-    int c_squared = (a*a) + (b*b);
-    return c_squared;
-}
