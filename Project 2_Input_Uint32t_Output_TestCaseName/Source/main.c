55b4b5d31faa9648db3f6733213cc3139c1c0b58
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-09-24 10:25:23 -0400

    Update main.c

--- Project 2_Input_Uint32t_Output_TestCaseName/Source/main.c
+++ Project 2_Input_Uint32t_Output_TestCaseName/Source/main.c
@@ -1,32 +1,33 @@
 // stdio.h is a header file in C that provides a set of functions for input/output operations
 #include <stdio.h>
 // Custom header file with definitions for called function and structs
-#include "TestID_Lookup.h"
+#include "ID_Lookup.h"
 
 int main()
 {
     //Declare input value
-    uint32_t test_input_value;
+    uint32_t ID_input_value;
 
     // Ask user to enter in a uint32_t value
-    printf("Please enter in a uint32_t test case value: ");
+    printf("Please enter in a uint32_t ID value: ");
 
     // Take the users input
-    scanf("%u", &test_input_value);
+    scanf("%u", &ID_input_value);
 
-    // call the getter function for the test name value
-    const char* Test_Name = Get_Test_Name(test_input_value);
+    // call the getter function for the ID name value
+    const char* ID_Name = Get_ID_Name(ID_input_value);
 
     // Logic behind return value
-    // If Test_Name returns a non-NULL value
-    if(Test_Name != NULL)
+    // If ID_Name returns a non-NULL value
+    if(ID_Name != NULL)
     {
-        printf("The test name associated with your input is: %s", Test_Name);
+        printf("The ID name associated with your input is: %s", ID_Name);
     }
     else
     {
-        printf("The test input value that was entered does not correspond to any test cases");
+        printf("The input value that was entered does not correspond to any ID values");
     }
 return 0;
 }
+
 
