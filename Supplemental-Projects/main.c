26fd2d8c0f205e990579484f15ed86e41123c68d
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-10-03 22:44:30 -0400

    Add files via upload

---
+++ Supplemental-Projects/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declare an age variable
+    int age = 28;
+
+    // Declare a name variable
+    char name[] = "Gerrick";
+
+    // Create a pointer variable for age
+    int *p1 = &age;
+
+    // Create a pointer variable for name
+    char *p2 = &name[0];
+
+    // Print the value of both variables
+    printf("The age of the user is: %i \n", age);
+    printf("The name of the user is: %s\n", name);
+
+    // Print the memory address of both variables
+    printf("The memory address of age is: %p\n", p1);
+    printf("The memory address of name is: %p\n", p2);
+
+    return 0;
+}
