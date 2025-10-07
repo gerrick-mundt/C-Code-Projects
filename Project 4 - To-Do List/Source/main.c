a8f51a6467bdb33f7b3e0298f64e1579e58db4d7
Gerrick Mundt <gerrick.d.mundt@gmail.com>
2025-10-07 10:01:17 -0400

    Update main.c

--- Project 4 - To-Do List/Source/main.c
+++ Project 4 - To-Do List/Source/main.c
@@ -16,18 +16,21 @@ int main ()
     // User input loop
     while(counter_1 < max_length)
     {
-        // Request user input
+        // Request user input using `fgets` with the array, max chars and stdin as input
         printf("List item %d: ", counter_1 + 1);
         char *result = fgets(buffer[counter_1], max_chars, stdin); // input buffer
+
+        // Remove the newline `\n` to eliminate unexpected behavior when printing / comparing strings
         buffer[counter_1][strcspn(buffer[counter_1], "\n")] = 0; // Remove (\n)
 
-        // Case where user input NULL (string length = 0)
+        // DEFINE CASES
+        // Case 1: User inputs NULL (string length = 0)
         if(strlen(buffer[counter_1]) == 0)
         {
             break;
         }
 
-        // Case where user input 0 (string input = exactly 0)
+        // Case 2: User input 0 (string input = exactly 0)
         // strcmp compares the string the user just typed string: strcmp(buffer[counter_1]
         // to the string we are checking it against: "0"
         else if(strcmp(buffer[counter_1],"0") == 0)
@@ -35,7 +38,7 @@ int main ()
             break;
         }
 
-        // Nominal case
+        // Case 3: User entered a string
         else
         {
             counter_1++;
@@ -46,7 +49,7 @@ int main ()
     printf("\n");
     printf("Your finalized to-do list has been generated - \n");
     
-    // When use input is complete, print list
+    // When user input is complete, print list
     for(counter_2 = 0; counter_2 < counter_1; counter_2++)
     {
         printf("To-do item %d: %s \n", counter_2 + 1, buffer[counter_2]);
